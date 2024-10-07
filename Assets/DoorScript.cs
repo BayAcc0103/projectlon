a16970cd84d344c13569c28d7a9fadf9680a5a1c
BayAcc0103 <145135982+BayAcc0103@users.noreply.github.com>
2024-10-07 15:23:35 +0700

    update door

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+    private Animator _animator;
+    private bool _isOpen = false;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Character"))
+        {
+            if (!_isOpen)
+            {
+                OpenDoor();
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Character"))
+        {
+            StartCoroutine(CloseDoorAfterDelay(3f)); // Đặt thời gian đóng cửa
+        }
+    }
+
+    void OpenDoor()
+    {
+        _isOpen = true;
+        _animator.SetBool("open", true);
+    }
+
+    IEnumerator CloseDoorAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _isOpen = false;
+        _animator.SetBool("open", false);
+    }
+}
