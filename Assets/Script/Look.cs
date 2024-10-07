94ccb9626002f7cfdd20a4f6f66120919874c9fd
verylowpower <doudqwer@gmail.com>
2024-10-07 23:04:09 +0700

    camera movement + jump

---
+++ Assets/Script/Look.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.OpenXR.NativeTypes;
+
+public class Look : MonoBehaviour
+{
+	public Camera cam;
+	public float xRotation = 0.0f;
+	
+	public float xSens = 30.0f;
+	public float ySens = 30.0f;
+	
+	public void ProcessLook(Vector2 input)
+	{
+		float mouseX = input.x;
+		float mouseY = input.y;
+		
+		xRotation = -(mouseY * Time.deltaTime)* ySens;
+		xRotation = Mathf.Clamp(xRotation,-80f,80f);
+		
+		cam.transform.localRotation = Quaternion.Euler(xRotation,0,0);
+		
+		transform.Rotate(mouseX * Time.deltaTime * xSens * Vector3.up);
+	}
+	
+}
--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Moved : MonoBehaviour
 {
 	private CharacterController controller;
 	private Vector3 playerVelocity;
+	private bool isGrounded;
 	public float speed = 5.0f;
+	public float gravity = 0.0f;
+	public float jumpForce = 0.0f;
+
 	void Start()
 	{
 		controller = GetComponent<CharacterController>();
@@ -15,13 +20,30 @@ public class Moved : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		
+		isGrounded = controller.isGrounded;	
 	}
 	
 	public void ProcessMove(Vector2 input)
 	{
-		Vector3 MoveDirection = new Vector3(input.x, 0, input.y); ;
+		Vector3 MoveDirection = Vector3.zero;
+		MoveDirection.x = input.x;
+		MoveDirection.z = input.y;
 		
 		controller.Move(transform.TransformDirection(MoveDirection)*speed*Time.deltaTime);
+		playerVelocity.y += gravity	*Time.deltaTime;
+		if(isGrounded&& playerVelocity.y < 0)
+		{
+			playerVelocity.y = -2;
+		}
+		controller.Move(playerVelocity*Time.deltaTime);
+		
+		
+	}
+	public void Jump()
+	{
+		if(isGrounded)
+		{
+			playerVelocity.y = Mathf.Sqrt(jumpForce *-3.0f *gravity);
+		}
 	}
 }
