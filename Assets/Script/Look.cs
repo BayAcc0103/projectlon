662575bd2718499614a65f0af130576f93755531
verylowpower <doudqwer@gmail.com>
2024-10-08 22:07:14 +0700

    Update Look.cs

--- Assets/Script/Look.cs
+++ Assets/Script/Look.cs
@@ -10,19 +10,20 @@ public class Look : MonoBehaviour
     public float xSens = 30.0f;
     public float ySens = 30.0f;
 
-  
+   
     public void ProcessLook(Vector2 input)
     {
         float mouseX = input.x;
         float mouseY = input.y;
 
-        
-       	xRotation = -(mouseY * Time.deltaTime)* ySens;
-        xRotation = Mathf.Clamp(xRotation, -80f, 80f); 
+     
+        xRotation -= (mouseY * Time.deltaTime) * ySens; 
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);  
 
+        
         cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
 
-        
+      
         transform.Rotate(Vector3.up * mouseX * Time.deltaTime * xSens);
     }
 }
