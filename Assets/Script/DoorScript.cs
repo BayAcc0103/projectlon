daaa6d6cbc1a499b3d06c0102f1caf4c817ec87c
BayAcc0103 <145135982+BayAcc0103@users.noreply.github.com>
2024-10-30 23:29:58 +0700

    add sound effect & fix linh tinh

--- Assets/Script/DoorScript.cs
+++ Assets/Script/DoorScript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour {
     private Animator _animator;
     private bool _isOpen = false;
+    public AudioSource _audioSource; // Thêm biến để lưu trữ AudioSource
+    public AudioClip doorSound; // Reference to the sound effect
+
 
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other)
@@ -34,6 +38,12 @@ public class DoorScript : MonoBehaviour {
     {
         _isOpen = true;
         _animator.SetBool("open", true);
+        if (_audioSource != null && doorSound != null)
+        {
+            _audioSource.PlayOneShot(doorSound);
+        }
+
+
     }
 
     IEnumerator CloseDoorAfterDelay(float delay)
