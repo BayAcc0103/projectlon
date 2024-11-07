1a9819a4bfc277595b56d340092ca314c91a70b9
quannghiem756 <62365443+quannghiem756@users.noreply.github.com>
2024-11-07 17:41:07 +0700

    Update ObjectDestroyer.cs

--- Assets/Script/ObjectDestroyer.cs
+++ Assets/Script/ObjectDestroyer.cs
@@ -17,37 +17,7 @@ public class ObjectDestroyer : MonoBehaviour
 		gameManager = FindObjectOfType<GameManager>();
 	}
 
-	public void Update()
-	{   	
-		float distanceToObject = Vector3.Distance(player.position, objectToDestroy.transform.position);
-		// Nếu người chơi ở trong khoảng cách yêu cầu
-		if (distanceToObject <= activationDistance)
-		{	
-			float triggerInput = Input.GetAxis("RightTrigger");
-			bool isKeyDown = Input.GetKey(KeyCode.E) || triggerInput > 0.1f;
-			// Kiểm tra xem người chơi có giữ phím E không
-			if (isKeyDown)
-			{
-				holdTime += Time.deltaTime; // Tăng thời gian giữ theo thời gian thực
-				// Cập nhật hiển thị thời gian giữ
-				holdTimeText.text = "" + Mathf.Ceil(requiredHoldTime - holdTime).ToString() + "s";
-				// Nếu người chơi giữ phím E đủ thời gian
-				if (holdTime >= requiredHoldTime)
-				{
-					DisableObject(); // Hủy đối tượng			
-				}
-			}
-			else
-			{
-				holdTime = 0f; // Reset lại thời gian giữ nếu nhả phím
-				holdTimeText.text = ""; // Xóa hiển thị khi không giữ phím
-			}
-		}
-		else
-		{
-			holdTimeText.text = ""; // Xóa hiển thị khi ra ngoài khoảng cách
-		}
-	}
+	
 
 	private void DisableObject()
 	{	
